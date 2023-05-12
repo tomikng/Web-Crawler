c0b8ca66cfd9301994f9f9c8ca0d7b979b195906
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2023-05-12 15:44:35 +0200

    fixed bug, that it creates two executions, but we only need one

--- backend/webcrawler/tasks.py
+++ backend/webcrawler/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from django.utils import timezone
+from django.utils.datetime_safe import datetime
 
 from api.models import WebsiteRecord, Execution, CrawledPage
 from .crawler import Crawler
@@ -9,7 +10,16 @@ from .crawler import Crawler
 def crawl_website(website_record_label):
     website_record = WebsiteRecord.objects.get(label=website_record_label)
     crawler = Crawler(website_record)
-    execution = Execution.objects.create(website_record=website_record)
+    execution, created = Execution.objects.get_or_create(
+        website_record=website_record,
+        defaults={
+            'status': 'pending',
+            'start_time': datetime.now(),
+            'end_time': None,
+            'num_sites_crawled': 0
+        }
+    )
+
     crawler.crawl(website_record.url)
 
     # Update the Execution model with status and other required information
