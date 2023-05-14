ef766f653574b9fcb4654942b6647900c28964a3
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-14 21:38:19 +0200

    update

--- frontend/src/components/Content/CrawledPages/CrawledPages.jsx
+++ frontend/src/components/Content/CrawledPages/CrawledPages.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './CrawledPages.css';
-import axios from 'axios';
 
 const CrawledPages = () => {
-  useEffect(() => {
-    axios.post('http://127.0.0.1:8000/api/', {
-      query: `
-        query {
-            websites {
-            identifier
-            label
-            url
-            regexp
-            tags
-            active
-            }
-        }
-      
-      `
-    })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
+ 
   return (
     <div>
-      CrawledPages
+        CrawledPages
     </div>
   );
 }
--- frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
+++ frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './WebsiteRecords.css';
+import axios from 'axios';
 
 const WebsiteRecords = () => {
+
+  useEffect(() => {
+    axios.post('http://127.0.0.1:8000/api/', {
+      query: `
+        query {
+            websites {
+            identifier
+            label
+            url
+            regexp
+            tags
+            active
+            }
+        }
+      
+      `
+    })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
        WebsiteRecords
