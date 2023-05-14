a10ccc8ea4021f7ffa330c6372af3ae80f4afba7
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2023-05-14 19:59:23 +0200

    change enpoint graphql to api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('api/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('webcrawler/', include('webcrawler.urls')),
 ]
