2ae3fbcd4e6d65ec3d9177a1f353b1a69091483b
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2023-05-15 15:41:01 +0200

    Added simple validation

--- frontend/src/components/Content/Crawl/Crawl.jsx
+++ frontend/src/components/Content/Crawl/Crawl.jsx
@@ -26,39 +26,90 @@ const Crawl = () => {
   };
 
   const saveWebsiteRecord = (data) => {
-      const base_url = 'http://127.0.0.1:8000/api';
-      const endpoint = '/website_records/create/';
-      const url = base_url + endpoint;
+    const base_url = 'http://127.0.0.1:8000/api';
+    const endpoint = '/website_records/create/';
+    const url = base_url + endpoint;
 
-      axios
-          .post(url, data)
-          .then(response => {
-            console.log('Data saved successfully:', response.data);
-            // Handle successful response here
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            // Handle error here
+    axios
+      .post(url, data)
+      .then(response => {
+        console.log('Data saved successfully:', response.data);
+        // Handle successful response here
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        // Handle error here
       });
-
-
-
   };
 
+  const validateForm = () => {
+    const urlInput = document.getElementById('crawl_url');
+    const regexInput = document.getElementById('crawl_regex');
+    const url = urlInput.value.trim();
+    const regex = regexInput.value.trim();
+    const periodicity = document.getElementById('crawl_periodicity').value;
+    const label = document.getElementById('crawl_label').value;
+
+    if (url === '') {
+      alert('Please enter a URL');
+      return false;
+    }
+
+    if (!isValidUrl(url)) {
+      alert('Please enter a valid URL');
+      urlInput.focus();
+      return false;
+    }
+
+    if (regex === '') {
+      alert('Please enter a boundary regexp');
+      return false;
+    }
+
+    if (!isValidRegex(regex)) {
+      alert('Please enter a valid boundary regexp');
+      regexInput.focus();
+      return false;
+    }
+
+    if (periodicity.trim() === '') {
+      alert('Please select a periodicity');
+      return false;
+    }
+
+    if (label.trim() === '') {
+      alert('Please enter a label');
+      return false;
+    }
+
+    return true;
+  };
+
+  const isValidUrl = (url) => {
+    const urlRegex = /^(https?:\/\/)?([\w.-]+)(\.[\w.-]+)\/?$/;
+    return urlRegex.test(url);
+  };
+
+  const isValidRegex = (regex) => {
+    try {
+      new RegExp(regex);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
 
   const crawl = () => {
+    if (!validateForm()) {
+      return;
+    }
+
     const url = document.getElementById('crawl_url').value;
     const regex = document.getElementById('crawl_regex').value;
     const periodicity = document.getElementById('crawl_periodicity').value;
     const label = document.getElementById('crawl_label').value;
     const active = document.getElementById('crawl_active').checked;
 
-    // console.log('URL:', url);
-    // console.log('Boundary regexp:', regex);
-    // console.log('Periodicity:', periodicity);
-    // console.log('Label:', label);
-    // console.log('Active:', active);
-    // console.log('Tags:', tags);
     const data = {
       url: url,
       boundary_regexp: regex,
@@ -69,7 +120,7 @@ const Crawl = () => {
     };
 
     saveWebsiteRecord(data);
-  }
+  };
 
 
   return (
