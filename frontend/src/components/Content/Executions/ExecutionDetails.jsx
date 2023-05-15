28528140d1c36dbedf4aa05dcbd9d55bc0445655
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2023-05-15 19:13:23 +0200

    implemented execution details

--- frontend/src/components/Content/Executions/ExecutionDetails.jsx
+++ frontend/src/components/Content/Executions/ExecutionDetails.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import './ExecutionDetails.css'; // Import the CSS file for styling
 
 const ExecutionDetails = () => {
   const { id } = useParams();
+  const [execution, setExecution] = useState(null);
+
+  const BASE_URL = 'http://127.0.0.1:8000/api/executions/';
+
+  useEffect(() => {
+    const fetchExecutionDetails = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}${id}/`);
+        const data = response.data;
+        setExecution(data);
+      } catch (error) {
+        console.error('Error fetching execution details:', error);
+      }
+    };
+
+    fetchExecutionDetails();
+  }, [id]);
 
   return (
-    <div>
-      <h2>Execution Details - ID: {id}</h2>
-      {/* Implement the logic to fetch and display the details for the execution record with the given ID */}
+    <div className="execution-details">
+      <h2 className="execution-details__title">Execution Details - ID: {id}</h2>
+      {execution ? (
+        <div className="execution-details__content">
+          <div className="execution-details__field">
+            <strong>ID:</strong> {execution.id}
+          </div>
+          <div className="execution-details__field">
+            <strong>Status:</strong> {execution.status}
+          </div>
+          <div className="execution-details__field">
+            <strong>Start Time:</strong> {execution.start_time}
+          </div>
+          <div className="execution-details__field">
+            <strong>End Time:</strong> {execution.end_time}
+          </div>
+          <div className="execution-details__field">
+            <strong>Number of Sites Crawled:</strong> {execution.num_sites_crawled}
+          </div>
+          <div className="execution-details__field">
+            <strong>Website Record:</strong> {execution.website_record}
+          </div>
+        </div>
+      ) : (
+        <p>Loading execution details...</p>
+      )}
     </div>
   );
 };
