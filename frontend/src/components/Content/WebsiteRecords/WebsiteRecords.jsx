9133c5c26931e31b12a2ef17ba90d265d6d9e5bb
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-15 21:15:42 +0200

    Website records table

--- frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
+++ frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
@@ -1,36 +1,102 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './WebsiteRecords.css';
 import axios from 'axios';
 
+
+const base_url = 'http://127.0.0.1:8000/api';
+
+const fetchExecutions = async () => {
+  try {
+    const response = await axios.get(`${base_url}/executions/`);
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+const fetchWebsiteRecords = async (id) => {
+  try {
+    const response = await axios.get(`${base_url}/website_records/${id}/`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching website records:', error);
+    return null;
+  }
+};
+
+const processWebsiteRecords = async (records) => {
+  const processedRecords = [];
+  for (const record of records) {
+    const websiteRecord = await fetchWebsiteRecords(record.website_record);
+    if (websiteRecord) {
+      const { label } = websiteRecord;
+      processedRecords.push({
+        id: record.id,
+        websiteRecord: websiteRecord.id,
+        label:label,
+        url: websiteRecord.url,
+        periodicity: websiteRecord.periodicity,
+        status: record.status,
+        tags: websiteRecord.tags,
+        start_time: new Date(record.start_time).toLocaleString(),
+        status: record.status
+      });
+    }
+  }
+  return processedRecords;
+};
+
+
 const WebsiteRecords = () => {
+  
+  const [wesiteRecords, setWesiteRecords] = useState([]);
 
   useEffect(() => {
-    axios.post('http://127.0.0.1:8000/api/', {
-      query: `
-      query {
-        websites {
-          identifier
-          label
-          url
-          regexp
-          tags
-          active
-        }
+    const fetchData = async () => {
+      const executionData = await fetchExecutions();
+      if (executionData) {
+        const processedData = await processWebsiteRecords(executionData);
+        setWesiteRecords(processedData);
       }
-      `
-    })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <div>
-        CrawledPages
+        <div className="websites-records-container">
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Label</th>
+                  <th>Url</th>
+                  <th>Periodicity</th>
+                  <th>Tags</th>
+                  <th>Last time execution</th>
+                  <th>Status last execution</th>
+                </tr>
+              </thead>
+              <tbody>
+                {wesiteRecords.map((wesiteRecord) => (
+                  <tr key={wesiteRecord.websiteRecord}>
+                    <td>{wesiteRecord.websiteRecord}</td>
+                    <td>
+                      <a href={`/executions/${wesiteRecord.id}`}>{wesiteRecord.label}</a>
+                    </td>
+                    <td>{wesiteRecord.url}</td>
+                    <td>{wesiteRecord.periodicity}</td>
+                    <td>{wesiteRecord.tags}</td>
+                    <td>{wesiteRecord.start_time}</td>
+                    <td>{wesiteRecord.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
     </div>
   );
 
