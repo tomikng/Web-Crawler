4b3ffb41f24b0703a63f819a22e506f6be428fe6
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-15 22:03:16 +0200

    sort website records by url

--- frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
+++ frontend/src/components/Content/WebsiteRecords/WebsiteRecords.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './WebsiteRecords.css';
 import axios from 'axios';
 
-
 const base_url = 'http://127.0.0.1:8000/api';
 
 const fetchExecutions = async () => {
@@ -26,7 +25,7 @@ const fetchWebsiteRecords = async (id) => {
   }
 };
 
-const processWebsiteRecords = async (records) => {
+const processWebsiteRecords = async (records, sortBy) => {
   const processedRecords = [];
   for (const record of records) {
     const websiteRecord = await fetchWebsiteRecords(record.website_record);
@@ -35,7 +34,7 @@ const processWebsiteRecords = async (records) => {
       processedRecords.push({
         id: record.id,
         websiteRecord: websiteRecord.id,
-        label:label,
+        label: label,
         url: websiteRecord.url,
         periodicity: websiteRecord.periodicity,
         status: record.status,
@@ -45,61 +44,73 @@ const processWebsiteRecords = async (records) => {
       });
     }
   }
+
+  processedRecords.sort((a, b) => {
+    if (sortBy === 'url') {
+      return a.url.localeCompare(b.url);
+    } else if (sortBy === '-url') {
+      return b.url.localeCompare(a.url);
+    }
+    return 0;
+  });
+
   return processedRecords;
 };
 
-
 const WebsiteRecords = () => {
-  
   const [wesiteRecords, setWesiteRecords] = useState([]);
+  const [sortedBy, setSortedBy] = useState('url');
 
   useEffect(() => {
     const fetchData = async () => {
       const executionData = await fetchExecutions();
       if (executionData) {
-        const processedData = await processWebsiteRecords(executionData);
+        const processedData = await processWebsiteRecords(executionData, sortedBy);
         setWesiteRecords(processedData);
       }
     };
 
     fetchData();
-  }, []);
+  }, [sortedBy]);
+
+  const sortIcon = sortedBy === 'url' ? <span>&#x25BC;</span> : <span>&#x25B2;</span>;
 
   return (
     <div>
-        <div className="websites-records-container">
-            <table>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Label</th>
-                  <th>Url</th>
-                  <th>Periodicity</th>
-                  <th>Tags</th>
-                  <th>Last time execution</th>
-                  <th>Status last execution</th>
-                </tr>
-              </thead>
-              <tbody>
-                {wesiteRecords.map((wesiteRecord) => (
-                  <tr key={wesiteRecord.websiteRecord}>
-                    <td>{wesiteRecord.websiteRecord}</td>
-                    <td>
-                      <a href={`/website_records/${wesiteRecord.websiteRecord}`}>{wesiteRecord.label}</a>
-                    </td>
-                    <td>{wesiteRecord.url}</td>
-                    <td>{wesiteRecord.periodicity}</td>
-                    <td>{wesiteRecord.tags}</td>
-                    <td>{wesiteRecord.start_time}</td>
-                    <td>{wesiteRecord.status}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+      <div className="websites-records-container">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Label</th>
+              <th onClick={() => setSortedBy(sortedBy === 'url' ? '-url' : 'url')}>
+                Url {sortIcon}
+              </th>
+              <th>Periodicity</th>
+              <th>Tags</th>
+              <th>Last time execution</th>
+              <th>Status last execution</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wesiteRecords.map((wesiteRecord) => (
+              <tr key={wesiteRecord.websiteRecord}>
+                <td>{wesiteRecord.websiteRecord}</td>
+                <td>
+                  <a href={`/website_records/${wesiteRecord.websiteRecord}`}>{wesiteRecord.label}</a>
+                </td>
+                <td>{wesiteRecord.url}</td>
+                <td>{wesiteRecord.periodicity}</td>
+                <td>{wesiteRecord.tags}</td>
+                <td>{wesiteRecord.start_time}</td>
+                <td>{wesiteRecord.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-
 };
 
 export default WebsiteRecords;
