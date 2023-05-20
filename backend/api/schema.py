7237e27b5d789cfb7661a70999de8f0b4c2b0935
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-20 16:10:00 +0200

    Schema update

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -4,29 +4,33 @@ from .models import WebsiteRecord, CrawledPage
 from django.utils import timezone
 
 
-class WebPageType(DjangoObjectType):
-    identifier = graphene.ID(source='pk')
-    regexp = graphene.String(source='boundary_regexp')
+class WebPage(DjangoObjectType):
+    identifier = graphene.ID(source='pk', required=True)
+    regexp = graphene.String(source='boundary_regexp', required=True)
 
     class Meta:
         model = WebsiteRecord
         fields = ("label", "url", "tags", "active")
 
 
-class NodeType(DjangoObjectType):
+class Node(DjangoObjectType):
     class Meta:
         model = CrawledPage
         fields = ("title", "url", "crawl_time", "links")
 
-    owner = graphene.Field(WebPageType)
+    owner = graphene.Field(WebPage, required=True)
+    crawl_time = graphene.String()
 
     def resolve_owner(self, info):
         return self.execution.website_record
 
+    def resolve_crawl_time(self, info):
+        return str(self.crawl_time)
+
 
 class Query(graphene.ObjectType):
-    websites = graphene.List(WebPageType)
-    nodes = graphene.List(NodeType, web_pages=graphene.List(graphene.ID))
+    websites = graphene.List(graphene.NonNull(WebPage), required=True)
+    nodes = graphene.List(graphene.NonNull(Node), web_pages=graphene.List(graphene.NonNull(graphene.ID)), required= True)
 
     def resolve_websites(root, info):
         return WebsiteRecord.objects.all()
@@ -35,6 +39,7 @@ class Query(graphene.ObjectType):
         if web_pages:
             return CrawledPage.objects.filter(execution__website_record__in=web_pages)
         return CrawledPage.objects.all()
+
 
 
 schema = graphene.Schema(query=Query)
