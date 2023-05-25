101eb73c8dcd427d887789658db43914de66f1ee
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2023-05-25 15:20:41 +0200

    Fixed issue #30 - select default website record

--- frontend/src/components/Content/CrawledPages/SelectWebsite.jsx
+++ frontend/src/components/Content/CrawledPages/SelectWebsite.jsx
@@ -16,12 +16,15 @@ const fetchWebsiteRecords = async () => {
 
 const SelectWebsite = () => {
   const navigate = useNavigate();
-  const [selectedWebsite, setSelectedWebsite] = useState("");
+  const [selectedWebsite, setSelectedWebsite] = useState(null);
   const [websiteRecords, setWebsiteRecords] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchWebsiteRecords();
+      if (data && data.length > 0) {
+        setSelectedWebsite(data[0].id);
+      }
       setWebsiteRecords(data);
     };
 
@@ -38,10 +41,12 @@ const SelectWebsite = () => {
     <form onSubmit={handleSubmit}>
       <label>
         Select a website:
-        <select value={selectedWebsite} onChange={e => setSelectedWebsite(e.target.value)}>
-          {websiteRecords.map(record => (
-            <option key={record.id} value={record.id}>{record.label} - {record.url}</option>
-          ))}
+        <select value={selectedWebsite || ''} onChange={e => setSelectedWebsite(e.target.value)}>
+          {
+            websiteRecords.map(record => (
+              <option key={record.id} value={record.id}>{record.label} - {record.url}</option>
+            ))
+          }
         </select>
       </label>
       <input type="submit" value="Go to Visualization" />
